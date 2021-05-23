15c6af4bff458aa4cbc8958ecf8f90440a983743
Saurav Lal <sauravkumarlal7060@gmail.com>
2021-05-23 20:16:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3300;
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
