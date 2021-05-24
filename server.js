08ded75402d861990e262f732d699ac4d09771f6
Saurav Lal <sauravkumarlal7060@gmail.com>
2021-05-24 19:14:06 +0530

    added and design page layout

--- server.js
+++ server.js
@@ -6,16 +6,28 @@ const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 
 // Assets
-app.use(express.static('public'))
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.use(express.static("public"));
 
 // set template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
