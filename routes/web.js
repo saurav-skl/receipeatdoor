4344b4a38dd8912a8299484564d7b034019eccb4
Saurav Lal <sauravkumarlal7060@gmail.com>
2021-05-27 15:22:35 +0530

    built login and register auth

--- routes/web.js
+++ routes/web.js
@@ -2,17 +2,23 @@ const authControllers = require("../app/http/controllers/authControllers");
 const cartControllers = require("../app/http/controllers/customers/cartControllers");
 const homeControllers = require("../app/http/controllers/homeControllers");
 
+// Middleware
+
+const guest = require("../app/http/middleware/guest");
+
 function initRoutes(app) {
   app.get("/", homeControllers().index);
 
-  
-  app.get("/login", authControllers().login);
-  
-  app.get("/register", authControllers().register);
-  
+  app.get("/login", guest, authControllers().login);
+  app.post("/login", authControllers().postLogin);
+
+  app.get("/register", guest, authControllers().register);
+  app.post("/register", authControllers().postRegister);
+
+  app.post("/logout", authControllers().logout);
+
   app.get("/cart", cartControllers().index);
   app.post("/update-cart", cartControllers().update);
-
 }
 
 module.exports = initRoutes;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
 
 // Database connections
 const url =
@@ -47,17 +48,24 @@ app.use(
   })
 );
 
+// Passport Config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // Assets
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: false }));
 
 // Global Middleware
 
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 });
 
@@ -71,5 +79,5 @@ require("./routes/web")(app);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   var timestamp = new Date().toLocaleString();
-  console.log(timestamp)
+  console.log(timestamp);
 });
